d72b02037a59d62b50c55578b6390546f2ad7923
dongzhihan <529211933@qq.com>
2017-04-06 14:44:44 +0800

    2017年4月6日14:44:36 更新

--- src/js/api.js
+++ src/js/api.js
@@ -22,17 +22,17 @@
    },
    login: "PDA.asmx/Login", //登陆
    GetEngDashboardInfo: "PDA.asmx/GetEngDashboardInfo", //调拨二维码
-   StockTransfer: "PDA.asmx/StockTransfer", //调拨提交
+   StockTransfer: "SAPB1.asmx/StockTransfer", //调拨提交
    GetShipPlan: "PDA.asmx/GetShipPlan", //出货计划
-   Ship:"PDA.asmx/Ship",   //出货提交
+   Ship: "PDA.asmx/Ship", //出货提交
    GetProductionDashboardInfo: "PDA.asmx/GetProductionDashboardInfo", //获取生产看板信息
    MoInput: "PDA.asmx/MoInput", //投入
    MoOutput: "PDA.asmx/MoOutput", //产出
    CreateProductionReceipt: "PDA.asmx/CreateProductionReceipt", //产出入库
    CreateProductionOrder: "PDA.asmx/CreateProductionOrder", //生产订单创建提交
-   GetRecevingInfo: "PDA.asmx/GetRecevingInfo", //入库
+   GetRecevingInfo: "SAPB1.asmx/GetRecevingInfo", //入库
    CreatePoGoodsReceipt: "PDA.asmx/CreatePoGoodsReceipt", //入库提交
-   GetMoInput:"PDA.asmx/ GetMoInput", //投入详情
-   GetMoInput:"PDA.asmx/ GetMoInput" //产出详情
+   GetMoInput: "PDA.asmx/ GetMoInput", //投入详情
+   GetMoInput: "PDA.asmx/ GetMoInput" //产出详情
  } ////////////api存放位置
  export default api;
